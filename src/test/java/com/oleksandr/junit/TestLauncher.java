7a7337c32a465430a659b87ec7aed309ef4de79c
Kulbit Oleksandr <kulbitsanya0@gmail.com>
2025-04-02 16:27:10 +0200

    Testing LauncherAPI

---
+++ src/test/java/com/oleksandr/junit/TestLauncher.java
@@ -0,0 +1,37 @@
+package com.oleksandr.junit;
+
+import com.oleksandr.junit.service.UserServiceTest;
+import org.junit.platform.engine.discovery.DiscoverySelectors;
+import org.junit.platform.launcher.LauncherDiscoveryRequest;
+import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
+import org.junit.platform.launcher.core.LauncherFactory;
+import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
+
+import java.io.PrintWriter;
+
+public class TestLauncher {
+
+    public static void main(String[] args) {
+        var launcher = LauncherFactory.create();
+
+        // launcher.registerLauncherDiscoveryListeners();
+        // launcher.registerTestExecutionListeners();
+
+        var summaryGeneratingListener = new SummaryGeneratingListener(); // to see statistics
+        //launcher.registerTestExecutionListeners(summaryGeneratingListener);
+
+        LauncherDiscoveryRequest discoveryRequest =
+                LauncherDiscoveryRequestBuilder
+                        .request()
+                        .selectors(DiscoverySelectors.selectClass(UserServiceTest.class))
+                        .selectors(DiscoverySelectors.selectPackage("com.oleksandr.junit"))
+                        .build();
+        launcher.execute(discoveryRequest,summaryGeneratingListener);
+
+        try(var writer = new PrintWriter(System.out);) {
+            summaryGeneratingListener.getSummary().printTo(writer);
+        }
+    }
+
+}
+
